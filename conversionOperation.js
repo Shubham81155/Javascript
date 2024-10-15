1ea08b3a51b15d170fd33d305fec222e3cf39a28
Shubham81155 <shubham81155@gmail.com>
2024-10-15 23:44:19 +0530

    conversion of datatypes

--- conversionOperation.js
+++ conversionOperation.js
@@ -20,3 +20,38 @@ let somenumber= 33
 let StringNumber = String(somenumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+/****************** Operation***************/
+let value=3
+let negvalue= -value
+console.log(negvalue);
+
+console.log(2*2);
+console.log(2-2);
+console.log(2+2);
+console.log(2/2);
+console.log(2**4);
+console.log(2%2);
+
+let str1 ="hello"
+let str2= " Shubham"
+let str3= str1+str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1 +"2");
+console.log("1"+ 2 +2);
+console.log(1+ 2 +"2");
+
+console.log(true);
+console.log(+true);
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter);
+git commit
