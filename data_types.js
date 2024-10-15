b65fa422b71c53ebf14c1610faa940b3e7feb379
Shubham81155 <shubham81155@gmail.com>
2024-10-15 23:08:45 +0530

    datatypes

---
+++ data_types.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newer version
+
+//alert("3+3")//we are using node js
+
+console.log(3+3)
+console.log("Shubham")
+
+let name ="Dibyajyothi"
+let age =20
+let isloggedIn = false
+let state;
+
+
+// number => 2 to the power 53
+//bigint 
+//String=> ""/''
+//boolean => true/false
+//null => standalone type
+//undefined =>
+//symbol => unique
+
+
+//object
+console.log(typeof("shubham"));
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
