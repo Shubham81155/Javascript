232f441a66a89980b4531a7437c66bc7b1627755
Shubham81155 <shubham81155@gmail.com>
2024-10-15 10:22:04 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,11 @@
+let accountId = 144553
+let accountEmail = "ahdsa@google.com"
+accountPassword = "12345"
+accountCity ="jaipur"
+let accountState;
+console.log(accountId);
+
+accountEmail = "ah@google.com"
+accountPassword = "21232"
+accountCity ="Bengaluru"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
