ccf65bb9d8e47fd099947a95a96c260695996654
Shubham81155 <shubham81155@gmail.com>
2024-10-16 18:50:49 +0530

    datatypes_summary

---
+++ datatypes_summary.js
@@ -0,0 +1,34 @@
+// primitive
+// 7 types : String, Number, Boolean, null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 325432364545634564n
+
+
+
+//Reference(Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Spiderman","Salman","Ajay"]
+let myobj={
+    name:"Shubham",
+    Age: 21,
+}
+
+const myFunction = function()
+{
+console.log("Hello World");
+
+}
+console.log(typeof outsideTempr);
