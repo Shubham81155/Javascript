1b5476600cfd5bbd80d5f69f64d63ad48594db99
Shubham81155 <shubham81155@gmail.com>
2024-10-25 23:20:02 +0530

    array

--- Array2.js
+++ Array2.js
@@ -1,5 +1,7 @@
-const marvel_heros =["thor","Ironman","Psiderman"]
+const marvel_heros =["thor","Ironman","siderman"]
 const dc_heros = ["superman","flash","batman"]
 
 marvel_heros.push(dc_heros)
 console.log(marvel_heros);
+console.log(marvel_heros.concat(dc_heros));
+
